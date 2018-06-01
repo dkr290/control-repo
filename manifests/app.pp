d78e229f4936daac23295df38f2eb27b147ad45c
root <root@puppet.codespaces.io>
2018-06-01 11:28:28 +0000

    creating specs for dev environment

--- manifests/app.pp
+++ manifests/app.pp
@@ -1,4 +1,4 @@
-node 'node1' {
+node 'node3' {
 
     class {'tomcat':
     
@@ -22,39 +22,6 @@ node 'node1' {
 
 }
 
-node 'node2' {
-
-      class {'tomcat':
-    
-      xms           =>"64m",
-      xmx           => "128m",
-      service_state => 'running'
-    }
-    
-    tomcat::deploy {'sysfoo':
-    
-      deploy_url => 'https://1-135286305-gh.circle-artifacts.com/0/tmp/circle-artifacts.PKBa5ZP/sysfoo.war',
-      checksum => 'md5',
-      checksum_value => 'be3ecf5f67648577532e7f0a8791e8e8',
-    
-    
-    }
-    
-   
-
-
-}
 
 
 
-node default {
-  notify { '##### Default node ####':
-     
-  message => '
-     Applying the default Block
-     There is no definition for that particular node.
-  
-  '
-  }
-
-}
